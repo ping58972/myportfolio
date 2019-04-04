88d215b3dc18f1b9bc585ebe94ac7e2c5165de3e
ping58972 <ping58972@gmail.com>
2019-04-04 01:16:45 -0500

    fixed side page to show and hide

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,32 @@
 const symbol_btn = document.querySelector(".symbol_btn");
 const close_line = document.querySelector(".close_line");
 const aside = document.querySelector("aside");
+const bodyPage = document.querySelector(".main_projects");
+const bodyPage1 = document.querySelector(".top_left");
+const bodyPage2 = document.querySelector(".other_project");
+const bodyPage3 = document.querySelector("footer");
+
 symbol_btn.addEventListener('click', openSide);
 close_line.addEventListener('click', closeSide);
+bodyPage.addEventListener('click', bodyCloseSide);
+bodyPage1.addEventListener('click', bodyCloseSide);
+bodyPage2.addEventListener('click', bodyCloseSide);
+bodyPage3.addEventListener('click', bodyCloseSide);
 
-//let showSide = true;
+let showSide = true;
 function openSide(){
-   // if(showSide){
         aside.classList.add('show');
-      //  let showSide = false;
-   // }else{
-     //   aside.classList.remove('show');
-    //    let showSide = true;
-  //  }
+         showSide = false;
+    
 }
 
 function closeSide() {
-   //// if(showSide){
     aside.classList.remove('show');
-   // }
+   showSide = true;
+}
+
+function bodyCloseSide(){
+     if(!showSide){
+       closeSide();
+     }
 }
