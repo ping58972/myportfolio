8a0675ea18f0f2063123596e33f550a6d0f100f3
ping58972 <ping58972@gmail.com>
2019-04-05 04:05:18 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ const bodyPage = document.querySelector(".main_projects");
 const bodyPage1 = document.querySelector(".top_left");
 const bodyPage2 = document.querySelector(".other_project");
 const bodyPage3 = document.querySelector("footer");
+const strDescribe = document.getElementsByClassName("str_describe");
+const mainProject = document.getElementsByClassName("project_name");
+const mainProject_sub = document.querySelector(".project_name.current");
+const strDescribe_sm = document.getElementsByClassName("str_describe_sm");
+
 
 symbol_btn.addEventListener('click', openSide);
 close_line.addEventListener('click', closeSide);
@@ -12,6 +17,25 @@ bodyPage.addEventListener('click', bodyCloseSide);
 bodyPage1.addEventListener('click', bodyCloseSide);
 bodyPage2.addEventListener('click', bodyCloseSide);
 bodyPage3.addEventListener('click', bodyCloseSide);
+ mainProject_sub.addEventListener('click', displayProject_stop);
+
+
+let x=0;
+
+for(let i=0; i<mainProject.length; i++){
+ 
+  mainProject[i].addEventListener('click', () => {
+    strDescribe[i].classList.add('current');
+    mainProject[i].classList.add('current');
+   strDescribe_sm[i].classList.add('current');   
+    if(i!==x){
+      strDescribe[x].classList.remove('current');
+    mainProject[x].classList.remove('current');
+    strDescribe_sm[x].classList.remove('current');
+    }  
+    x=i;
+  });
+}
 
 let showSide = true;
 function openSide(){
@@ -30,3 +54,11 @@ function bodyCloseSide(){
        closeSide();
      }
 }
+
+
+function displayProject_stop(){
+  mainProject_sub.removeEventListener('click', displayProject_stop); 
+  event.stopPropagation();
+}
+
+
