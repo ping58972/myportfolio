3121d3f39832c9d8113a54b7d8ed15250036cc5a
ping58972 <ping58972@gmail.com>
2019-04-09 15:49:49 -0500

    Fixed other project to swiper slideshow

--- dist/js/main.js
+++ dist/js/main.js
@@ -61,4 +61,20 @@ function displayProject_stop(){
   event.stopPropagation();
 }
 
-
+//import Swiper from 'swiper';
+//var Swiper = require('swiper');
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+ // slidesPerGroup: 4,
+  //loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
