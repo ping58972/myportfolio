45268b9844bcdae6fedd81a7983dd3ad22e53fe5
ping58972 <ping58972@gmail.com>
2019-07-11 13:58:55 -0500

    add readme.md

---
+++ dist/README.md
@@ -0,0 +1,58 @@
+# My Portfolio
+
+<a href="https://ping58972.github.io/myportfolio/"><img src="https://a.imge.to/2019/07/12/qvRn6.png" target="_blank" alt="Reaponse Website"></a>
+<a href="https://ping58972.github.io/myportfolio/"><img src="https://a.imge.to/2019/07/12/qvBmr.png" target="_blank" alt="Reaponse Website"></a>
+<a href="https://ping58972.github.io/myportfolio/"><img src="https://a.imge.to/2019/07/12/qv2Hq.png" target="_blank" alt="Reaponse Website"></a>
+<a href="https://ping58972.github.io/myportfolio/"><img src="https://a.imge.to/2019/07/12/qvhSO.png" target="_blank" alt="Reaponse Website"></a>
+
+The purpose of website is for looking jobs about software engineering, web developer, and programmer. The website features include: showing my resume, side projects and my skill, etc.
+
+## Built with
+
+- [HTML5](#) - The web framework used.
+- [SASS](#) - The web framework used.
+- [Javascript](#) - Used to implement MongoDB.
+- [Swiper](http://idangero.us/swiper/get-started) Used for displaying the project's carts.
+- More detail... see package.json
+
+## Getting Started
+
+### Prerequisites
+
+Nodejs with npm - lastest version.
+
+```
+Nodejs 12.6.0
+```
+
+### Installing
+
+Download or clone the repository, then
+
+```
+npm install
+```
+
+And start open the projects for developing. It will go to http://localhost:9000
+
+```
+npm run sass
+```
+
+## Deployment
+
+or you can go to <a href="https://ping58972.github.io/myportfolio/">personal website</a> that had been deployed to show the result of this project.
+
+## Authors
+
+- **(Ping) Nalongsone Daddank** - _Initial work_ - [ping58972](https://github.com/ping58972)
+
+## License
+
+This project is licensed under the MIT License
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,10 @@
-const symbol_btn = document.querySelector(".symbol_btn");
-const close_line = document.querySelector(".close_btn");
-const aside = document.querySelector("aside");
-const bodyPage = document.querySelector(".main_projects");
-const bodyPage1 = document.querySelector(".top_left");
-const bodyPage2 = document.querySelector(".other_project");
-const bodyPage3 = document.querySelector("footer");
-
+const symbol_btn = document.querySelector('.symbol_btn');
+const close_line = document.querySelector('.close_btn');
+const aside = document.querySelector('aside');
+const bodyPage = document.querySelector('.main_projects');
+const bodyPage1 = document.querySelector('.top_left');
+const bodyPage2 = document.querySelector('.other_project');
+const bodyPage3 = document.querySelector('footer');
 
 symbol_btn.addEventListener('click', openSide);
 close_line.addEventListener('click', closeSide);
@@ -15,62 +14,59 @@ bodyPage2.addEventListener('click', bodyCloseSide);
 bodyPage3.addEventListener('click', bodyCloseSide);
 
 let showSide = true;
-function openSide(){
-        aside.classList.add('show');
-         showSide = false;
-    
+function openSide() {
+  aside.classList.add('show');
+  showSide = false;
 }
 
 function closeSide() {
-    aside.classList.remove('show');
-   showSide = true;
+  aside.classList.remove('show');
+  showSide = true;
 }
 
-function bodyCloseSide(){
-     if(!showSide){
-       closeSide();
-     }
+function bodyCloseSide() {
+  if (!showSide) {
+    closeSide();
+  }
 }
 
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
-    spaceBetween: 30,
+  slidesPerView: 4,
+  spaceBetween: 30,
   // slidesPerGroup: 4,
-    //loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  //loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
 var swiper = new Swiper('.swiper-container-main', {
-        effect: 'coverflow',
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-      // loop: true,
-        autoplay: {
-          delay: 1500,
-        // disableOnInteraction: false,
-        },
-        coverflowEffect: {
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows : true,
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-          renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (index + 1) + '</span>';
-          },
-        },
-    });
-
-   
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  // loop: true,
+  autoplay: {
+    delay: 1500
+    // disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function(index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    }
+  }
+});
